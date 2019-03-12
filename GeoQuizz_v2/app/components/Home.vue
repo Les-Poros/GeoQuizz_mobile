3b33a7e489a97de1c7cb25445ca4d295f1710009
Maeva <butaye.maeva@gmail.com>
2019-03-12 11:08:17 +0100

    ajout de la localisation avec la prise de photo

--- GeoQuizz_v2/app/components/Home.vue
+++ GeoQuizz_v2/app/components/Home.vue
@@ -6,8 +6,9 @@
 			<Button text="Choisir une photo" @tap="selectPicture" />
 			<Button text="voir localisation" @tap="GetLocationTap"/>
 			<WrapLayout>
-				<Image v-for="img in images" :src="img.src" width="75" height="75" />
+				<Image v-for="img in images" :src="img['src']" width="75" height="75" />
 			</WrapLayout>
+			<!--<label v-for="img in imagesAvecLoc" :text="img['loc']['lat']" ></label>-->
         </StackLayout>
     </Page>
 </template>
@@ -19,13 +20,16 @@ import * as imagepicker from "nativescript-imagepicker";
 import { Image } from "tns-core-modules/ui/image";
 import { isEnabled, enableLocationRequest, getCurrentLocation, watchLocation, distance, clearWatch } from "nativescript-geolocation";
 var geolocation = require("nativescript-geolocation");
+
 export default {
 	data() {
 		return {
-			images:[]
+			images:[],
+			localisation: [],
 		}
 	},
 	methods:{
+		// Méthode qui permet de séléctionner une photo dans l'album
 		selectPicture() {
 
 			let context = imagepicker.create({
@@ -43,7 +47,9 @@ export default {
 
 					let img = new Image();
 					img.src = selected;
-					this.images.push(img);
+
+					let tabImage = {'src' : selected, 'loc' : ''};
+					this.images.push(tabImage);
 				});
 			}).catch(function (e) {
 				console.log('error in selectPicture', e);
@@ -51,17 +57,17 @@ export default {
 
 		},
 		takePicture() {
+		// Méthode qui permet de prendre une photo
+			this.GetLocationTap();
 			camera.requestPermissions()
 			.then(() => {
 				camera.takePicture({ width: 300, height: 300, keepAspectRatio: true, saveToGallery:false })
 				.then(imageAsset => {
-					//let tabImage = [];
 					let img = new Image();
 					img.src = imageAsset;
-					//let location = GetLocationTap();
-					//tabImage = {'src' : imageAsset, 'loc' : location};
-					//this.images.push(tabImage);
-					this.images.push(img);
+
+					let tabImage = {'src' : imageAsset, 'loc' : this.localisation};
+					this.images.push(tabImage);
 					console.log('ive got '+this.images.length+' images now.');
 				})
 				.catch(e => {
@@ -71,16 +77,19 @@ export default {
 			.catch(e => {
 				console.log('Error requesting permission');
 			});
-		},GetLocationTap() {
+		},
+		// Méthode qui permet de géolocaliser le portable
+		GetLocationTap() {
+			var self = this;
+			var lat = 0;
+			var long = 0;
 			geolocation.enableLocationRequest();
 			var location = geolocation.getCurrentLocation({desiredAccuracy: 3, updateDistance: 10, maximumAge: 20000, timeout: 20000}).
 			then(function(location) {
-				if (location) {
-					alert("Current location is: " + location.latitude + " " + location.longitude);
-					return [location.latitude,location.longitude];
-				}
-			}, function(e){
-				console.log("Error: " + e.message);
+					//alert("Current location is: " + location.latitude + " " + location.longitude);
+					lat = location.latitude;
+					long = location.longitude;
+					self.localisation = {'lat' : lat, 'long' : long};
 			});
 		}
 	}
